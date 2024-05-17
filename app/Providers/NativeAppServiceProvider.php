f2123b99ed760c499dc61c6101a800195df63ffb
Jordi Huertas <jordihuertas@hotmail.es>
2024-05-17 12:23:05 +0200

    Configured NativeAppServiceProvider (php.ini config)

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -22,6 +22,11 @@ class NativeAppServiceProvider implements ProvidesPhpIni
     public function phpIni(): array
     {
         return [
+            'memory_limit' => '512M',
+            'display_errors' => '1',
+            'error_reporting' => 'E_ALL',
+            'max_execution_time' => '0',
+            'max_input_time' => '0',
         ];
     }
 }
