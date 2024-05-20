3a56ad795efd2cc3c27b2411e2edfb735e06acb3
Jordi Huertas <jordihuertas@hotmail.es>
2024-05-20 22:59:37 +0200

    Modified card styles so the user can see witch card is behind another

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,11 +2,28 @@
     @if(!$isHidden) draggable=true drag-item drop-item @endif
     >
     @if(!$isHidden)
-        <i class="icon bi bi-suit-{{ $typeName }}-fill float-end"></i>
+        <div class="row poker-card--top m-0">
+            <div class="col text-start p-0">
+                <span class="icon-number">{{ $number }}</span>
+            </div>
+            <div class="col text-end p-0">
+                <i class="icon bi bi-suit-{{ $typeName }}-fill"></i>
+            </div>
+        </div>
     @endif
-    <div class="d-flex align-items-center justify-content-center text-center poker-card--number">
-        <span class="gradient-text">@if(!$isHidden){{ $number }}@endif</span>
+    <div class="row d-flex align-items-center justify-content-center text-center poker-card--number m-0">
+        <span class="col gradient-text">@if(!$isHidden){{ $number }}@endif</span>
     </div>
+    @if(!$isHidden)
+        <div class="row poker-card--bottom m-0 align-items-end">
+            <div class="col text-end p-0">
+                <span class="icon-number">{{ $number }}</span>
+            </div>
+            <div class="col text-start p-0">
+                <i class="icon bi bi-suit-{{ $typeName }}-fill"></i>
+            </div>
+        </div>
+    @endif
 </div>
 
 {{--<span class="small text-secondary">Deck: {{ $cardDeck }} - Pos: {{ $cardPosition }}</span>--}}
